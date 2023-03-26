81e18f76421a27d21b2f2ed2a66890a765a18300
İrem Çelebi <iremcelebi2404@gmail.com>
2023-03-26 14:54:26 +0300

    PORT DEĞİŞİKLİĞİ

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // require your server and launch it
 const server = require("./api/server");
-const port = 9001;
+const port = process.env.PORT || 9001;
 
 server.listen(port, () => console.log("API running on port " + port));
